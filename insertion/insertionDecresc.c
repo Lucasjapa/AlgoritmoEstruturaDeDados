08629477e03ee0401437626e3aed8e5f393ada41
Lucas Cavalcante <lucascdsilva.bj@gmail.com>
2022-11-02 21:32:13 -0300

    Algoritmo InsertionSort Ordem Decrescente
    Ajuste no algoritmo de ordem Crescente e Decrescente.

--- insertion/insertionDecresc.c
+++ insertion/insertionDecresc.c
@@ -1,34 +1,35 @@
-// InsertionSort Crescente.
+// InsertionSort Decrescente.
 
 #include <stdio.h>
 
 int main(void)
 {
-    int v[] = {59, 26, 41, 100, 89, 2};
-    // int v[] = {31, 41, 59, 26, 41, 58};
-    // int v[] = {59, 41, 26};
-    int aux, j, i, maxSize;
+
+    long v[] = {59, 26, 41, 100, 89, 2};
+    long aux, j, i, maxSize;
 
     maxSize = (sizeof v / sizeof v[0]) - 1;
     // printf("%d", maxSize);
 
     for (i = 0; i < maxSize; i++)
     {
-        if (v[i] > v[i + 1])
+        aux = 0;
+        if (v[i + 1] > v[i])
         {
-            aux = v[i];
-            v[i] = v[i + 1];
-            v[i + 1] = aux;
+            aux = v[i + 1];
+            v[i + 1] = v[i];
+            v[i] = aux;
         }
         j = i + 1;
 
         while (j > 0)
         {
-            if (v[j] < v[j - 1])
+            aux = 0;
+            if (v[j - 1] < v[j])
             {
-                aux = v[j];
-                v[j] = v[j - 1];
-                v[j - 1] = aux;
+                aux = v[j - 1];
+                v[j - 1] = v[j];
+                v[j] = aux;
             }
             j--;
         }
@@ -37,6 +38,7 @@ int main(void)
     // Tirar Duvida com o professor sobre o valor da posição zero do vetor.
     for (i = 0; i <= maxSize; i++)
     {
+        // printf("%d:", i);
         printf("%d ", v[i]);
     }
 }
